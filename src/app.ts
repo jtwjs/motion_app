fb39225ff20913d11be45bdbaffd8c26a6f7018f
Jung taeWoong <60641307+jtwjs@users.noreply.github.com>
2021-06-30 22:22:52 +0900

    Add Containe, PageItem Component & Delete Button

---
+++ src/app.ts
@@ -0,0 +1,32 @@
+import {PageComponent, Composable} from "./components/page/page.js";
+import {ImageComponent} from "./components/page/item/image.js";
+import {VideoComponent} from "./components/page/item/videos.js";
+import {NoteComponent} from "./components/page/item/note.js";
+import {TodoComponent} from "./components/page/item/todo.js";
+import {Component} from "./components/component.js";
+
+
+
+class App {
+	private readonly page: Component & Composable;
+	constructor(appRoot: HTMLElement) {
+		this.page = new PageComponent();
+		this.page.attachTo(appRoot);
+
+		const image = new ImageComponent('Image Title', 'https://picsum.photos/600/300');
+		this.page.addChild(image);
+
+		const video = new VideoComponent('Video Title', 'https://www.youtube.com/watch?v=domBCXgtYxI&list=RDdomBCXgtYxI&start_radio=1');
+		this.page.addChild(video);
+
+		const note = new NoteComponent('Note Title', 'Note Body');
+		this.page.addChild(note);
+
+		const todo = new TodoComponent('Todo Title', 'Todo Item');
+		this.page.addChild(todo);
+
+
+	}
+}
+
+new App(document.querySelector('.document')! as HTMLElement);
