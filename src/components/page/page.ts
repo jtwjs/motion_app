395069160a346eb2ab0810eeeeb894e52dd570ee
Jung taeWoong <60641307+jtwjs@users.noreply.github.com>
2021-06-30 22:20:38 +0900

    clear git cache

--- src/components/page/page.ts
+++
@@ -1,7 +0,0 @@
-import { BaseComponent } from "../component.js";
-
-export class PageComponent extends BaseComponent<HTMLUListElement> {
-	constructor() {
-		super('<ul class="page">This is PageComponents<ul>');
-	}
-}
